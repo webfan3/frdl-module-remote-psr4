6e4c97801a4ffd4210c47605df84479c0b3e5536
Till Wehowski <github@webfan.de>
2020-08-23 21:08:15 +0200

    Update remote-psr4.php

--- level-2/configurations/remote-psr4.php
+++ level-2/configurations/remote-psr4.php
@@ -1,8 +1,31 @@
 <?php
 
+//$s['server'], $s['register'], $s['version'], $s['allowFromSelfOrigin'], $s['salted'], $s['classmap']
+$meta = json_decode(file_get_contents('https://discover.api.webfan.de/.well-known/frdlweb.json'));
+$servers = $meta->repository->psr4;
 
+$endpoints = [];
+
+foreach($servers as $server){
+   $endpoint=[
+      'server' => $server->url,
+      'register' => (isset($server->register)) ? $server->register : true,
+      'version' => (isset($server->register)) ? $server->version : 'latest',
+      'allowFromSelfOrigin' => (isset($server->allowFromSelfOrigin)) ? $server->allowFromSelfOrigin : false,
+      'salted' => (isset($server->salted)) ? $server->salted : true,
+      'classmap' => (isset($server->classmap) && is_array($server->classmap)) ? $server->classmap :  [	  
+        \Wehowski\Gist\Http\Response\Helper::class => 'https://gist.githubusercontent.com/wehowski/d762cc34d5aa2b388f3ebbfe7c87d822/raw/5c3acdab92e9c149082caee3714f0cf6a7a9fe0b/Wehowski%255CGist%255CHttp%255CResponse%255CHelper.php?cache_bust=${salt}',
+      ],
+     
+   ];
+   $endpoints[]=$endpoint;
+}
 
 return [
-
+  'api' => [
+       'endpoints' => [
+           'psr4' => $endpoints,
+       ],
+  ],
 
 ];
