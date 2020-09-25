4ac8ee548e295d8d071e8ba8650506413fb8ac3b
Till Wehowski <github@webfan.de>
2020-09-25 12:09:57 +0200

    Update autloader-file-include.php

--- inc/autloader-file-include.php
+++ inc/autloader-file-include.php
@@ -1,9 +1,18 @@
 <?php
 call_user_func(function(){
     //\Webfan\Psr4Loader\RemoteFromWebfan::getInstance('frdl.webfan.de', true, 'latest', true);
-    $apiConfig = require hiqdev\composer\config\Builder::path('api');
-  
-    \Webfan\Psr4Loader\RemoteFromWebfan::getInstance($apiConfig['api']['endpoints']['psr4'], true, 'latest', false, true,  [	   
-      \Wehowski\Gist\Http\Response\Helper::class => 'https://gist.githubusercontent.com/wehowski/d762cc34d5aa2b388f3ebbfe7c87d822/raw/5c3acdab92e9c149082caee3714f0cf6a7a9fe0b/Wehowski%255CGist%255CHttp%255CResponse%255CHelper.php?cache_bust=${salt}',
-    ]);
+    $classmap = [	          
+                \Wehowski\Gist\Http\Response\Helper::class => 'https://gist.githubusercontent.com/wehowski/d762cc34d5aa2b388f3ebbfe7c87d822/raw/5c3acdab92e9c149082caee3714f0cf6a7a9fe0b/Wehowski%255CGist%255CHttp%255CResponse%255CHelper.php?cache_bust=${salt}',          
+            ];
+    $configfile = \hiqdev\composer\config\Builder::path('api');
+    if(file_exists($configfile)){    
+        $apiConfig = require $configfile;    
+        if(is_array($apiConfig) && isset($apiConfig['api']['endpoints']['psr4'])){        
+            \Webfan\Psr4Loader\RemoteFromWebfan::getInstance($apiConfig['api']['endpoints']['psr4'], true, 'latest', false, true,  $classmap; 
+        }
+        
+        if(is_array($apiConfig) && isset($apiConfig['api']['endpoints']['remote_autoload'])){        
+            \Webfan\Psr4Loader\RemoteFromWebfan::getInstance($apiConfig['api']['endpoints']['remote_autoload'], true, 'latest', false, true,  $classmap); 
+        }
+    }
 });
